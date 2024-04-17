caa058c9662da0210cc9a4ae8c5df93c3e0e984d
daniahih <daniahih310@gmail.com>
2024-04-17 10:51:01 +0200

    added array

---
+++ js/array/array.js
@@ -0,0 +1,55 @@
+// const array = [];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const arr = ["dania", 1, true, [1, 3]]
+// // console.log(arr);
+// console.log(arr[3][0])
+// const fruits = ['apple', 'banana', 'orange'];
+// console.log(fruits.length)
+// console.log(fruits[2].length);
+
+// function push(n) {
+
+//     console.log(n)
+//     return n
+// }
+// const numbers = [1, 2, 3];
+// numbers[3] = 4
+// numbers.push(5, 6);
+// numbers.unshift(0)
+// numbers.unshift(-2, -1)
+// console.log(numbers)
+
+
+
+// const fruits = ['apple', 'banana', 'orange', 'grape'];
+// const lastFruit = fruits.pop();
+// const firstFruit = fruits.shift();
+// console.log(firstFruit);
+// console.log(fruits);
+// console.log(lastFruit);
+
+// const numbers = [1, 2, 3, 4, 5];
+
+
+// console.log(numbers.indexOf(3)); //2 
+// console.log(numbers.indexOf(6)); // error 
+// value undifened
+// -1 not found 
+// const numbers = [1, 2, 3, 4, 5];
+// const slice1 = numbers.slice(1, 4);
+// console.log(slice1); 
+// const fruits = ['apple', 'banana', 'orange'];
+// fruits.splice(2, 1);
+// fruits.splice(0, 0, "dania", "amin");
+// console.log(fruits);
+
+const array = [1, 'dania', 3, 4, 'amin', [1, 3], ['dania', 'rwaa'], true, ['mohammad', 1.5], Date.now(), 150, '150', [0.50, 'hussien']]
+console.log("first array", array);
+array.sort()
+console.log(array);
+Array
+Date
+const scheool = {
+    "name": 'hussin '
+}
+scheool.name
